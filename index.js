ded42d6abf404039ef88d4be24136af1f10e6cd3
Joachim Carlsen <joachim@joavps.(none)>
2014-12-19 20:34:36 +0100

    Refactor

--- index.js
+++ index.js
@@ -18,40 +18,50 @@ var form = {
     TRI : "A"
 }
 
+function getSecondURL(url) {
+
+	request.get(url, {}, function(error, response, html) {
+		var $ = cheerio.load(html);
+
+		var table = $("table:first-of-type");
+		table.find('tr:first-of-type').remove();
+		table.find('tr').each(function(index, value) {
+			var person = {};
+			$(value).children("td").each(function(index, value) {
+				switch(index) {
+					case 0:
+						var url = base_url+$(value).children("a").attr("href");
+						person.fullname=$(value).text();
+						request.get(url , function(error, response, html) {
+							var $ = cheerio.load(html);
+							person.extra = $(".texte_std2").text();
+							if(person.extra!='' && person.fullname!='') {
+								console.log(person);
+							}
+						});
+						break;
+					case 1:
+						person.parents=$(value).text();
+						break;
+				}
+			});
+		});
+	});
+}
+
+function getURL(url) {
+	request.get(url, {}, function(error, response, html) {
+		var $ = cheerio.load(html);
+		var url = base_url+$('meta').attr('content').substr(8);
+		getSecondURL(url);
+	});
+}
 app.get('/scrape', function(req, res) {
     request.post(post_url, {form : form},
         function(error, response, html) {
             var $ = cheerio.load(html);
             var url = base_url+$("FRAME").attr('src');
-            request.get(url, {}, function(error, response, html) {
-                var $ = cheerio.load(html);
-                var url = base_url+$('meta').attr('content').substr(8);
-                request.get(url, {}, function(error, response, html) {
-                    var $ = cheerio.load(html);
-                    $('tr').each(function(index, value) {
-                        if(index==0)return;
-                        var person = {};
-                        $(value).children("td").each(function(index, value) {
-                            switch(index) {
-                                case 0:
-                                    var url = base_url+$(value).children("a").attr("href");
-                                    person.fullname=$(value).text();
-                                    request.get(url , function(error, response, html) {
-                                        var $ = cheerio.load(html);
-                                        person.extra = $(".texte_std2").text();
-                                        if(person.extra!='' && person.fullname!='') {
-                                            console.log(person);
-                                        }
-                                    });
-                                    break;
-                                case 1:
-                                    person.parents=$(value).text();
-                                    break;
-                            }
-                        });
-                    });
-                });
-            });
+			getURL(url);
         });
     res.send("Started");
 });
