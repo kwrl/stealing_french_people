6afd90712ed193ff16d41ba0767e08d71dbca850
kwrl <haakongk@stud.ntnu.no>
2015-01-02 14:14:28 +0100

    Adding queries to db, executing them in batches

--- index.js
+++ index.js
@@ -3,64 +3,28 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var iconv = require('iconv-lite');
 var path = require('path');
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('scraping_france', 'scraper', 'asd123ert', {
-    dialect : "mysql",
-    port : 3306,
-    host : "10.0.0.48"
-});
+
+var BASE_URL = "http://genearmor.cotesdarmor.fr/moteur/";
+var POST_URL = BASE_URL+'gapatience.php';
 
 var app = express();
-var base_url = "http://genearmor.cotesdarmor.fr/moteur/";
-var post_url = base_url+'gapatience.php';
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-function getSecondURL(url) {
-	request.get(url, {}, function(error, response, html) {
-		var $ = cheerio.load(html);
+exports = module.exports = app;
 
-		var table = $("table:first-of-type");
-		table.find('tr:first-of-type').remove();
-		table.find('tr').each(function(index, value) {
-			var person = {};
-			$(value).children("td").each(function(index, value) {
-				switch(index) {
-					case 0:
-						var url = base_url+$(value).children("a").attr("href");
-						person.fullname=$(value).text();
-						request.get(url , function(error, response, html) {
-							var $ = cheerio.load(html);
-                            var raw_extra = $(".texte_std2").text().match(/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/);
-                            if(raw_extra!=null && raw_extra.length>0){
-							    person.death= raw_extra[0];
-                            }
-							if(person.death!='' && person.fullname!='') {
-								console.log(person);
-								save_person(person);
-							}
-						});
-						break;
-					case 1:
-						person.parents=$(value).text();
-						break;
-				}
-			});
-		});
-	});
-}
+var sequelize = new Sequelize('scraping_france', 'scraper', 'asd123ert', {
+    dialect : "mysql",
+    logging: false,
+    port : 3306,
+    host : "10.0.0.48"
+});
 
-function getURL(url) {
-	request.get(url, {}, function(error, response, html) {
-		var $ = cheerio.load(html);
-		var url = base_url+$('meta').attr('content').substr(8);
-		getSecondURL(url);
-	});
-}
 sequelize
     .authenticate()
     .complete(function(err) {
@@ -71,14 +35,99 @@ sequelize
         }
     });
 
-var Person = sequelize.define('Person', {
-    fullname : Sequelize.STRING,
-    parents : Sequelize.STRING,
-    death : { 
-        type : Sequelize.STRING,
+var PersonQuery = sequelize.define('PersonQuery', {
+    first_name : { type: Sequelize.STRING, allowNull : false },
+    last_name : { type : Sequelize.STRING, allowNull : false },
+    mother : Sequelize.STRING,
+    father : Sequelize.STRING,
+    year_of_birth : { type : Sequelize.INTEGER, allowNull : false }
+}, {
+    instanceMethods : {
+        search : function() { 
+            console.log("Performing query:\t" + this.first_name + " " + this.last_name);
+            var query = this;
+            var form = {
+                TRONYME : "",
+                PRENOM : this.first_name,
+                PATRONYME: this.last_name,
+                CONJOINT : "",
+                B1 : "Rechercher",
+                SOURCE : "0",
+                TYPE : "T",
+                TRI : "A"
+            }; 
+            request.post(POST_URL, {form : form},
+                function(error, response, html) {
+                    var $ = cheerio.load(html);
+                    var url = BASE_URL+$("FRAME").attr('src');
+                    query.step_one(url);
+                }
+            );
+        },
+        step_one :  function(url) {
+            var query = this;
+	        request.get(url, {encoding:null}, function(error, response, html) {
+		        var $ = cheerio.load(iconv.decode(new Buffer(html), 'ISO-8859-1'));
+		        var url = BASE_URL+$('meta').attr('content').substr(8);
+                query.step_two(url);
+	        });
+        },
+        step_two : function(url) {
+            var query = this;
+	        request.get(url, {encoding:null}, function(error, response, html) {
+		    var $ = cheerio.load(iconv.decode(new Buffer(html), 'ISO-8859-1'));
+		    var table = $("table:first-of-type");
+		    table.find('tr:first-of-type').remove();
+		    table.find('tr').each(function(index, value) {
+			var person = {};
+			$(value).children("td").each(function(index, value) {
+				switch(index) {
+					case 0:
+						var url = BASE_URL+$(value).children("a").attr("href");
+						person.fullname=$(value).text();
+						request.get(url, {encoding:null} ,function(error, response, html) {
+		                    var $ = cheerio.load(iconv.decode(new Buffer(html), 'ISO-8859-1'));
+                            var raw_extra = $(".texte_std2").text().match(/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/);
+                            if(raw_extra!=null && raw_extra.length>0){
+                                person.year_of_death = raw_extra[0].substr(6,4);
+                                person.month_of_death = raw_extra[0].substr(3,2);
+                                person.day_of_death = raw_extra[0].substr(0,2);
+                            }
+							if(person.death!='' && person.fullname!='' && person.year_of_death!=0) {
+								save_person_result(person, query);
+							}
+						});
+						break;
+					case 1:
+						person.parents=$(value).text();
+						break;
+				}
+			});
+		});
+	});
+} 
+    }
+});
+
+var PersonResult = sequelize.define('PersonResult', {
+    fullname : { type : Sequelize.STRING, allowNull : false },
+    parents : { type : Sequelize.STRING, allowNull : false },
+    year_of_death : { 
+        type : Sequelize.INTEGER,
+        allowNull : false
+    },
+    month_of_death: { 
+        type : Sequelize.INTEGER,
+        allowNull : false
+    },
+    day_of_death: { 
+        type : Sequelize.INTEGER,
         allowNull : false
     }
 });
+
+PersonQuery.hasMany(PersonResult);
+PersonResult.belongsTo(PersonQuery);
 
 sequelize
     .sync({force : true})
@@ -90,55 +139,45 @@ sequelize
             }
     });
 
-function save_person(person) {
-    Person.create(person)
+function save_person_result(person, query) {
+    var p = PersonResult.build(person);
+    p.setPersonQuery(query);
+    p
+        .save()
+        .catch(function(err){
+            console.log("Could not save person result.");
+        });
+}
+
+function save_person_query(person) {
+    PersonQuery.create(person)
         .complete(function(err){
             if(err) {
-                console.log("Could not save person.");
+                console.log("Could not save person query.");
             } else {
-                console.log("Saved a person.");
+                console.log("Saved a person query", person);
             }
         });
 }
 
-app.post('/search', function(req, res) {
-    console.log("Person received");
-    var form = {
-        TRONYME : "",
-        PRENOM : req.body.first_name,
-        PATRONYME: req.body.last_name,
-        CONJOINT : "",
-        B1 : "Rechercher",
-        SOURCE : "0",
-        TYPE : "T",
-        TRI : "A"
-    };
-    console.log(form);
-    request.post(post_url, {form : form},
-        function(error, response, html) {
-            var $ = cheerio.load(html);
-            var url = base_url+$("FRAME").attr('src');
-			getURL(url);
+app.post('/add_query', function(req, res) {
+    console.log("Received new query");
+    save_person_query(req.body);
+});
+
+app.post('/init_search', function(req, res) {
+    console.log("Starting search.");
+    PersonQuery.findAll().on('success', function(queries) {
+        queries.forEach(function(query) {   
+            query.search();
         });
-    
-    res.send("Started");
+    });
 });
 
 app.get('/', function(reg, res) {
-    res.render('home', {title : "Skriv inn navn da.."});
+    res.render('home', {title : "Stealing french people"});
 });
 
 app.use("/static", express.static(path.join(__dirname,'static')));
-
-
 app.listen('8081');
-exports = module.exports = app;
-
-
-
-
-
-
-
-
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,45 +1,22 @@
-/*
- *  person = {
- *      last_name,
- *      first_name,
- *      year_of_birth
- *  }
- */
-
-
-function interpret_line(line) {
-    var items = line.split(" ");
-    var names = "";
-    for(var i=2;i<items.length;i++) {
-        names+=items[i]+' ';
-    }
-    return {
-        year_of_birth : items[0],
-        last_name : items[1],
-        first_name : names,
-    };
-}
-
-
 $(document).ready(function() {
-    $("#initiate").click(function() {
-        var lines = $("#input_data").val().split('\n');
-        for(var line in lines) {
+    $('#initiate').click(function() {
+        var raw = $('#input_data').val();
+        var persons = JSON.parse(raw);
+        for(var person in persons) {
             (function(i) {
                 setTimeout(function() {
-                    var person = interpret_line(lines[i]);
-                    console.log("First name:\t",person.first_name);
-                    console.log("Last name:\t",person.last_name);
-                    console.log("Year of birth:\t",person.year_of_birth);
-                    if(person!=null) {
-                        $.post("/search", person)
-                        .success(function(){
-                        }) 
-                        .fail(function(){
-                        });
-                    }
-                }, 1000*i);
-            }(line));
+                $.post("/add_query", persons[person])
+                    .success(function() {
+                    })
+                    .fail(function() {
+                    });
+                }, 100*i);
+            })(person);
         }
+    });
+    $('#search').click(function() {
+        $.post('/init_search','')
+            .success(function(){
+            });
     });
 });
